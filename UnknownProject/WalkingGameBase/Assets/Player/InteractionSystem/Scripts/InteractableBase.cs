10d1e6d2d73c125438e9b86476eb10356ac1be3a
umilton-git <ulysses.github@protonmail.com>
2021-06-05 14:33:30 -0500

    Fixed name, removed item system

---
+++ UnknownProject/WalkingGameBase/Assets/Player/InteractionSystem/Scripts/InteractableBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VHS 
+{
+public class InteractableBase : MonoBehaviour, Interactable
+{
+#region Variables
+  [Header("Interactable Settings")]
+ 
+  [Space]
+  public bool IsInteractable;
+  public bool MultipleUse;
+#endregion
+
+#region Properties
+  public bool isInteractable => IsInteractable;
+  public bool multipleUse => MultipleUse;
+#endregion
+
+#region Methods
+
+  public virtual void OnInteract()
+  {
+      Debug.Log("INTERACTED: " + gameObject.name);
+  }
+  #endregion
+}
+}
